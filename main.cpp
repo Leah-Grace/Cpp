47afbaf36f7eeef255d8a6d10b70d9645ba6591d
Leah Grace <Leahgdg@hotmail.com>
2022-12-04 20:02:23 -0500

    randomized numbers using time()

--- main.cpp
+++ main.cpp
@@ -1,19 +1,18 @@
 #include <iostream>
+#include <cstdlib>
+#include <cmath>
 
 using namespace std;
 
 int main() {
-        int a;
-        int b;
-        cout << "Enter value for a";
-        cin >> a;
-        cout << "Enter value for b";
-        cin >> b;
-        int tempB = b;
-        b = a;
-        a = tempB;
-        cout << a;
-        cout << b;
+
+    srand(time(0));
+    int limit = 20;
+    int number = rand() % limit;
+
+
+    cout << "srand number: " << number;
+
         return 0;
     }
 
