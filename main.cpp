e8a46554c150d1328f8fb4542c88061020a45847
Leah Grace <Leahgdg@hotmail.com>
2022-12-04 20:27:11 -0500

    program returns two rolls for 6-sided die

--- main.cpp
+++ main.cpp
@@ -1,17 +1,28 @@
 #include <iostream>
 #include <cstdlib>
+#include <ctime>
 #include <cmath>
 
 using namespace std;
 
 int main() {
+    const short lowerLimit = 1;
+    const short upperLimit = 6;
+    const short one = 1;
 
     srand(time(0));
-    int limit = 20;
-    int number = rand() % limit;
+    int dice1 = (rand() % (upperLimit - lowerLimit + one)) + one;
+    int dice2 = (rand() % (upperLimit - lowerLimit + one)) + one;
+    cout << "You rolled " << dice1 << " and " << dice2;
 
 
-    cout << "srand number: " << number;
+
+
+   // int dice2 = 0;
+    //int total = dice1 + dice2;
+
+
+    //cout << "You have rolled a  " << dice1 << " and a " << dice2 << " for a total of " << total;
 
         return 0;
     }
